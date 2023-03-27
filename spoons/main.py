f86b0772f3014edc6d4208ea84f2cbd19794d474
bensteinberg <bsteinberg@law.harvard.edu>
2023-03-27 07:24:52 -0400

    Log capture stderr

--- spoons/main.py
+++ spoons/main.py
@@ -205,6 +205,8 @@ def capture(vm, url, dryrun):
         if result.returncode == 0:
             cmd = f'sudo ignite cp { vm }:/root/archive.wacz /tmp/{ vm }.wacz'
             result = subprocess.run(shlex.split(cmd), capture_output=True)
+            for line in result.stderr:
+                logger.info(line)
             if result.returncode == 0:
                 return vm
     except Exception as e:  # which?
